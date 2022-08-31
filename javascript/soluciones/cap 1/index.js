62be04b3c605de114bc1dfd9e7851bdf44044689
Adrian <adrianaliguillen13@gmail.com>
2022-08-31 17:24:31 -0500

    dad

---
+++ javascript/soluciones/cap 1/index.js
@@ -0,0 +1,13 @@
+// number = 1
+// string = "text example"
+// bolean = true
+// bolean = false
+// var global , let específico, const pi
+// NaN = not an number ,Null definido como nada, undefined existe pero sin definir
+
+
+//----------------OPERADORES-------------------------------
+// += -= *= **= /= %= 
+if (true) {        
+    alert("hola")
+}
---
+++ javascript/soluciones/cap 2/index.js
@@ -0,0 +1,1 @@
+let array = ["banana","pera","manzana"];
